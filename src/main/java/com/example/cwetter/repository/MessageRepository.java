1953943aa77f6d080af4bfa29d981c5f71aeb820
Irina Marahtanova <Gnevsheva@netcracker.com>
2020-04-01 16:33:09 +0400

    new project. Cwitter add jpa(test hibernate) server bd postgreSQL 12.2

---
+++ src/main/java/com/example/cwetter/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.example.cwetter.repository;
+
+import com.example.cwetter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
